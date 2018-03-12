506079dcead918bb4db6621b8cc7e962216d9e17
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-03-12 03:49:26 -0700

    swapbit.c
    good

---
+++ swapbit.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    a=a^b;                                 
+    b=a^b;                                   
+    a=a^b;                                   
+    printf("a:%d\nb:%d",a,b);
+    return 0;
+}
